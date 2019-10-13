1c8f28f0c8c44ac664ec165b4369cffd295c6c73
Sam Gehman <sgehman@cs.uw.edu>
2019-10-12 22:47:23 -0700

    MASS COPYING

---
+++ app/src/main/java/com/busboard/busboard/core/nfc/TagReaderFactory.kt
@@ -0,0 +1,40 @@
+/*
+ * TagReaderFactory.kt
+ *
+ * This file is part of FareBot.
+ * Learn more at: https://codebutler.github.io/farebot/
+ *
+ * Copyright (C) 2017 Eric Butler <eric@codebutler.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.busboard.busboard.core.nfc
+
+import android.nfc.Tag
+import com.busboard.busboard.base.util.ByteUtils
+import com.busboard.busboard.card.TagReader
+import com.busboard.busboard.card.desfire.DesfireTagReader
+import com.busboard.busboard.key.CardKeys
+
+class TagReaderFactory {
+
+    fun getTagReader(
+        tagId: ByteArray,
+        tag: Tag
+    ): TagReader<*, *, *> = when {
+        "android.nfc.tech.IsoDep" in tag.techList -> DesfireTagReader(tagId, tag)
+        else -> throw UnsupportedTagException(tag.techList, ByteUtils.getHexString(tag.id))
+    }
+}
